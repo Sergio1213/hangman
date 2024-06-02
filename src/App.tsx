c60e4a973a02d28afc60077ee0d6091ee32e526e
Sergio Gabriel Galván Rodriguez <88905219+Sergio1213@users.noreply.github.com>
2024-06-01 18:57:33 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ const words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape', 'kiwi'];
 const appliances = ['refrigerator', 'oven', 'microwave', 'toaster', 'blender', 'vacuum', 'dryer'];
 const group=['onedirection','thesols','21savage','chayanne','joji']
 
-const allWords = [...words, ...appliances];
+const allWords = [...words, ...appliances,...group];
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <Hangman words={allWords} fruitsList={words} appliancesList={appliances} />
+      <Hangman  words={allWords} fruitsList={words} appliancesList={appliances} groupList{group} />
     </div>
   );
 }
