0542dee1cd21df64c73a429668cab89d11fc7c0e
Sergio Gabriel Galván Rodriguez <88905219+Sergio1213@users.noreply.github.com>
2024-06-01 19:28:22 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,17 @@ import Welcome from "./components/Welcome";
 
 const words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape', 'kiwi'];
 const appliances = ['refrigerator', 'oven', 'microwave', 'toaster', 'blender', 'vacuum', 'dryer'];
+const group=['onedirection','thesols','21savage','chayanne','joji'];
 
-
-const allWords = [...words, ...appliances];
+const allWords = [...words, ...appliances, ...group];
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <Hangman  words={allWords} fruitsList={words} appliancesList={appliances}  />
-
-
+      <Hangman  words={allWords} fruitsList={words} appliancesList={appliances} groupList={group} />
     </div>
   );
 }
 
-export default App;
+export default App; 
